2126942f5a7032f36edf44ecdf36d1adbf247445
Adiel Tsayag <adielts054@gmail.com>
2021-02-12 13:12:32 +0200

    add new 'best-c' plot

--- Final_Project.py
+++ Final_Project.py
@@ -5,8 +5,8 @@ from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
 
 
 # load, normalize and split the data
@@ -30,25 +30,27 @@ def read_normalize_and_split_data():
 def best_c(X_train, X_test, Y_train, Y_test):
     max_accuracy = 0
     max_accuracy_c = 0
-    accuracy_arr = []
-    c_arr = []
+    accuracy_arr = np.zeros(8)
+    c_arr = np.zeros(8)
     for i in range(-3, 5):
         logreg = LogisticRegression(penalty='l2', C=10 ** i, max_iter=len(X_train))
-        c_arr.append(10 ** i)
+        c_arr[i + 3] = 10 ** i
         logreg.fit(X_train, Y_train)
         Y_pred = logreg.predict(X_test)
         accuracy = metrics.accuracy_score(Y_test, Y_pred)
         if accuracy > max_accuracy:
             max_accuracy = accuracy
             max_accuracy_c = 10 ** i
-        accuracy_arr.append(accuracy)
+        accuracy_arr[i + 3] = accuracy
 
     # comparing c plot
-    plt.plot(c_arr, accuracy_arr, color='b')
-    plt.title("comparing c")
-    plt.xlabel("c")
-    plt.ylabel("accuracies")
-    # plt.show()
+    clrs = ['grey' if (x < max(accuracy_arr)) else 'green' for x in accuracy_arr]
+    plt.bar(np.arange(len(accuracy_arr)), 100 * accuracy_arr, color=clrs)
+    plt.xticks(np.arange(len(c_arr)), c_arr)
+    plt.ylabel('Accuracies (%)')
+    plt.xlabel("C's")
+    plt.title('Accuracy depends C')
+    plt.show()
 
     return max_accuracy_c
 
@@ -66,7 +68,7 @@ def Gaussian_Naive_Bayes(X_train, X_test, Y_train, Y_test):
     Y_pred = gnb.fit(X_train, Y_train).predict(X_test)
     accuracy_Gaussian_Naive_Bayes = metrics.accuracy_score(Y_test, Y_pred)
     print("accuracy Logistic Regression: " + str(accuracy_Gaussian_Naive_Bayes))
-    print("Number of mislabeled points out of a total %d points : %d" %(X_test.shape[0], (Y_test != Y_pred).sum()))
+    print("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (Y_test != Y_pred).sum()))
 
 
 def Random_Forest(X_train, X_test, Y_train, Y_test):
