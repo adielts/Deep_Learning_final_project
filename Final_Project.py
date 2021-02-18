545527d48095b84253d2d2d9c9c409492ed58fbe
Adiel Tsayag <adielts054@gmail.com>
2021-02-18 23:13:23 +0200

    add RFE algorithm and improve bestKSelect

--- Final_Project.py
+++ Final_Project.py
@@ -10,6 +10,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.feature_selection import RFE
+from sklearn.feature_selection import SelectKBest
 
 
 # load, normalize and split the data
@@ -55,7 +57,7 @@ def best_c(X_train, X_val, Y_train, Y_val):
     plt.xticks(np.arange(len(c_arr)), c_arr)
     plt.ylabel('Accuracies (%)')
     plt.xlabel("C's")
-    plt.title('Accuracy depends C')
+    plt.title('LR Accuracy depends C')
     plt.show()
 
     return max_accuracy_c
@@ -66,7 +68,7 @@ def Logistic_Regression(X_train, X_val, X_test, Y_train, Y_val, Y_test, X_train_
     c = best_c(X_train, X_val, Y_train, Y_val)
     print("best c: " + str(c))
 
-    logreg = LogisticRegression(penalty='l1',solver='saga', C=c, max_iter=len(X_train_val))
+    logreg = LogisticRegression(penalty='l1', solver='saga', C=c, max_iter=len(X_train_val))
     logreg.fit(X_train_val, Y_train_val)
     Y_pred = logreg.predict(X_test)
     accuracy_Logistic_Regression = metrics.accuracy_score(Y_test, Y_pred)
@@ -87,12 +89,12 @@ def Gaussian_Naive_Bayes(X_train_validation, X_test, Y_train_validation, Y_test)
 
 
 def Random_Forest(X_train_validation, X_test, Y_train_validation, Y_test):
-    # acc_arry = np.zeros(100)
-    # rand_state = np.arange(2, 202, 2)
-    # best_acc_and_ypred = np.zeros(2)
-    # for i in range(100):
-    #     rm = RandomForestClassifier(n_estimators=100, oob_score=True, n_jobs=-1, random_state=rand_state[i],
-    #                                 max_features=None, min_samples_leaf=20)
+    # acc_arry = np.zeros(20)
+    # rand_values = np.arange(10, 210, 10)
+    best_acc_and_ypred = np.array(2)
+    # for i in range(20):
+    #     rm = RandomForestClassifier(n_estimators=rand_values[i], n_jobs=-1, random_state=106, max_features=None,
+    #                                 min_samples_leaf=20)
     #     rm.fit(X_train_validation, Y_train_validation)
     #     Y_pred = rm.predict(X_test)
     #     acc = metrics.accuracy_score(Y_test, Y_pred)
@@ -105,7 +107,7 @@ def Random_Forest(X_train_validation, X_test, Y_train_validation, Y_test):
     Y_pred = rm.predict(X_test)
     acc = metrics.accuracy_score(Y_test, Y_pred)
 
-    # plt.plot(rand_state, acc_arry)
+    # plt.plot(rand_values, acc_arry)
     # plt.ylabel('Accuracy')
     # plt.xlabel("Number of Forests")
     # plt.title('Random Forest results')
@@ -113,7 +115,7 @@ def Random_Forest(X_train_validation, X_test, Y_train_validation, Y_test):
     print("Accuracy Random Forest: ", acc)
     print("Number of mislabeled points out of a total %d points : %d" % (
         X_test.shape[0], (Y_test != Y_pred).sum()))
-    build_confusion_matrix(Y_test, Y_pred, 'Random Forest confusion matrix')
+    # build_confusion_matrix(Y_test, Y_pred, 'Random Forest confusion matrix')
     return acc
 
 
@@ -151,10 +153,45 @@ def AdaBoost(X_train_validation, X_test, Y_train_validation, Y_test):
     return accuracy_AdaBoost
 
 
-def comparing_algorithms(accuracy_Logistic_Regression, accuracy_Gaussian_Naive_Bayes, accuracy_Random_Forest):
+def RFE_alg(X_train_validation, X_test, Y_train_validation, Y_test):
+    acc_arr = np.zeros(np.shape(X_train_validation)[1] + 1)
+    for i in range(np.shape(X_train_validation)[1], 1, -1):
+        rfecv = RFE(RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=106, max_features=None,
+                                           min_samples_leaf=20), n_features_to_select=i)
+        rfecv.fit(X_train_validation, Y_train_validation)
+        print('RFE ranking (', i, '):', rfecv.ranking_)
+        rfecv.fit(X_train_validation[:, np.argsort(rfecv.ranking_)[:i]], Y_train_validation)
+        score = rfecv.score(X_test[:, np.argsort(rfecv.ranking_)[:i]], Y_test)
+        print('Accuracy RFE is: ', score)
+        acc_arr[i] = score
+    acc_arr = acc_arr[2:]
+    plot_graph_acc_vs_n_fetures(np.arange(13, 1, -1), np.flip(acc_arr), 'RFE')
+
+
+def select_best_k(X_train_validation, X_test, Y_train_validation, Y_test):
+    acc_arr = np.zeros(np.shape(X_train_validation)[1] + 1)
+    for number_of_featurs in range(13, 0, -1):
+        print('*** run RF with best', number_of_featurs, 'selected featurs ***')
+        k_best = SelectKBest(k=number_of_featurs).fit(X_train_validation, Y_train_validation)
+        max_rank_indexes = np.argsort(-k_best.scores_)[:number_of_featurs]
+        acc_arr[number_of_featurs] = Random_Forest(X_train_validation[:, max_rank_indexes], X_test[:, max_rank_indexes],
+                                                   Y_train_validation, Y_test)
+    plot_graph_acc_vs_n_fetures(np.arange(13, 0, -1), np.flip(acc_arr[1:]), 'Select K-Best')
+
+
+def plot_graph_acc_vs_n_fetures(X_values, Y_values, title):
+    plt.plot(X_values, Y_values)
+    plt.gca().invert_xaxis()
+    plt.ylabel('Accuracy')
+    plt.xlabel("Number of featurs")
+    plt.title(title)
+    plt.show()
+
+
+def comparing_algorithms(accuracy_Logistic_Regression, accuracy_AdaBoost, accuracy_Random_Forest):
     # comparing accuracy plot
-    algorithms = ['Logistic Regression', 'Gaussian Naive Bayes', 'Random Forest']
-    accuracies = [accuracy_Logistic_Regression, accuracy_Gaussian_Naive_Bayes, accuracy_Random_Forest]
+    algorithms = ['Logistic Regression', 'AdaBoost', 'Random Forest']
+    accuracies = [accuracy_Logistic_Regression, accuracy_AdaBoost, accuracy_Random_Forest]
     xpos = np.arange(len(algorithms))
     plt.title("comparing accuracy")
     plt.xlabel("algorithms")
@@ -182,25 +219,32 @@ if __name__ == '__main__':
     X_train, X_validation, X_test, Y_train, Y_validation, Y_test, X_train_validation, Y_train_validation, labels = read_normalize_and_split_data()
 
     # Logistic Regression
-    accuracy_Logistic_Regression = Logistic_Regression(X_train.copy(), X_validation.copy(), X_test.copy(),
-                                                       Y_train.copy(), Y_validation.copy(), Y_test.copy(),
-                                                       X_train_validation.copy(), Y_train_validation.copy())
+    # accuracy_Logistic_Regression = Logistic_Regression(X_train.copy(), X_validation.copy(), X_test.copy(),
+    #                                                    Y_train.copy(), Y_validation.copy(), Y_test.copy(),
+    #                                                    X_train_validation.copy(), Y_train_validation.copy())
 
     # Gaussian Naive Bayes
-    accuracy_Gaussian_Naive_Bayes = Gaussian_Naive_Bayes(X_train_validation.copy(), X_test.copy(),
-                                                         Y_train_validation.copy(), Y_test.copy())
+    # accuracy_Gaussian_Naive_Bayes = Gaussian_Naive_Bayes(X_train_validation.copy(), X_test.copy(),
+    #                                                      Y_train_validation.copy(), Y_test.copy())
 
     # Random Forest
-    accuracy_Random_Forest = Random_Forest(X_train_validation.copy(), X_test.copy(), Y_train_validation.copy(),
-                                           Y_test.copy())
+    # accuracy_Random_Forest = Random_Forest(X_train_validation.copy(), X_test.copy(), Y_train_validation.copy(),
+    #                                        Y_test.copy())
 
     # Support Vector Classification
-    accuracy_SVC = Support_Vector_Classification(X_train_validation.copy(), X_test.copy(),
-                                                 Y_train_validation.copy(), Y_test.copy())
-
-    # comparing between algorithms
-    comparing_algorithms(accuracy_Logistic_Regression, accuracy_Gaussian_Naive_Bayes, accuracy_Random_Forest)
+    # accuracy_SVC = Support_Vector_Classification(X_train_validation.copy(), X_test.copy(),
+    #                                              Y_train_validation.copy(), Y_test.copy())
 
     # AdaBoost
-    accuracy_AdaBoost = AdaBoost(X_train_validation.copy(), X_test.copy(),
-                                 Y_train_validation.copy(), Y_test.copy())
+    # accuracy_AdaBoost = AdaBoost(X_train_validation.copy(), X_test.copy(), Y_train_validation.copy(),
+    #                              Y_test.copy())
+
+    # comparing between algorithms
+    # comparing_algorithms(accuracy_Logistic_Regression, accuracy_AdaBoost, accuracy_Random_Forest)
+
+    # RFE
+    # RFE_alg(X_train_validation.copy(), X_test.copy(), Y_train_validation.copy(), Y_test.copy())
+
+    # Select best K features
+    select_best_k(X_train_validation.copy(), X_test.copy(), Y_train_validation.copy(),
+                  Y_test.copy())
